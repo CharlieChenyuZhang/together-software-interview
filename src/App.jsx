29a44617b5b7de01ae8db907de336f8e6e7d2484
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2021-09-23 14:19:49 -0400

    remove unused files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffectf } from 'react'
+import styled from 'styled-components'
+
+const BaseDiv = styled.div``
+  
+const App = () => {
+  return <BaseDiv>Hello, World!</BaseDiv>;
+}
+
+export default App;
